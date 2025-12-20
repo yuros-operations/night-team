082f8a3f167a533cdd4184cc2721f8c572546f8d
Ibnudzaky <150795548+Ibnudzaky49@users.noreply.github.com>
2025-12-20 23:51:17 +0700

    edit && after command

--- boot/grub/docs.md
+++ boot/grub/docs.md
@@ -1,1 +1,17 @@
-
+# pseudocode dualboot-grub
+format root
+mounting root
+format boot
+mounting boot
+format swap
+mounting swap
+installasi package
+copy configurasi network dari installer ke /mnt
+pembuatan hostname
+konfigurasi waktu
+konfigurasi bahasa
+buat user
+install grub
+konfigurasi mkinitcpio
+penambahan entries
+generate grub
--- boot/grub/script.sh
+++ boot/grub/script.sh
@@ -12,13 +12,13 @@ homepath=/dev/sda9
 # root partition
 
 yes | mkfs.ext4 $procpath &&
-mount $procpath /mnt
+mount $procpath /mnt &&
 
 # linux partition
 
 yes | mkfs.ext4 $bootpath &&
 mkdir -p /mnt/boot &&
-mount $bootpath /mnt/boot
+mount $bootpath /mnt/boot &&
 
 # efi partition
 
@@ -26,28 +26,28 @@ mount $bootpath /mnt/boot
 # swap partition
 
 mkswap $swappath &&
-swapon $swappath
+swapon $swappath &&
 
 # home partition
 
 yes | mkfs.ext4 $homepath &&
 mkdir -p /mnt/home &&
-mount $homepath /mnt/home
+mount $homepath /mnt/home &&
 
 # package
 
 pacstrap /mnt base base-devel neovim linux-zen linux-firmware intel-ucode mkinitcpio efibootmgr os-prober grub iwd intel-ucode --noconfirm &&
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab &&
 
 # network
 
 cp /etc/systemd/network/* /mnt/etc/systemd/network &&
 mkdir -p /mnt/var/lib/iwd &&
-cp /var/lib/iwd/*.psk /mnt/var/lib/iwd
+cp /var/lib/iwd/*.psk /mnt/var/lib/iwd &&
 
 # hostname
 
-echo "$hostname" > /mnt/etc/hostname
+echo "$hostname" > /mnt/etc/hostname &&
 
 # time
 
@@ -56,24 +56,24 @@ arch-chroot /mnt hwclock --systohc &&
 arch-chroot /mnt timedatectl set-ntp true &&
 arch-chroot /mnt timedatectl set-timezone $timezone &&
 arch-chroot /mnt timedatectl status &&
-arch-chroot /mnt timedatectl show-timesync --all
+arch-chroot /mnt timedatectl show-timesync --all &&
 
 
 # locale
 
 arch-chroot /mnt sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8' /mnt/etc/locale.gen &&
-arch-chroot /mnt locale-gen
+arch-chroot /mnt locale-gen &&
 
 # user
 
 arch-chroot /mnt useradd -m $username &&
 arch-chroot /mnt echo $password | passwd $username --stdin &&
-echo "$username ALL=(ALL:ALL) ALL" > /mnt/etc/sudoers.d/nologin
+echo "$username ALL=(ALL:ALL) ALL" > /mnt/etc/sudoers.d/nologin &&
 
 # grub
 
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch --modules="tpm --disable-shim-lock" &&
-echo 'GRUB_DISABLE_OS_PROBER=false' >> /mnt/etc/default/grub
+echo 'GRUB_DISABLE_OS_PROBER=false' >> /mnt/etc/default/grub &&
 
 # mkinitcpio
 
@@ -83,7 +83,7 @@ mv /mnt/boot/*-ucode.img /mnt/boot/vmlinuz-linux-* /mnt/boot/kernel &&
 mv -f /mnt/etc/mkinitcpio.conf /mnt/etc/mkinitcpio.d/default.conf &&
 echo "#linux zen default" > /mnt/etc/mkinitcpio.d/default.conf &&
 export CPIOHOOK="base systemd autodetect microcode kms keyboard block filesystem fsck" &&
-printf "MODULE=()\nBINARIES=()\nFILES=()\nHOOKS=($CPIOHOOK)" >> /mnt/etc/mkinitcpio.d/default.conf 
+printf "MODULE=()\nBINARIES=()\nFILES=()\nHOOKS=($CPIOHOOK)" >> /mnt/etc/mkinitcpio.d/default.conf &&
 
 # efi
 
@@ -92,7 +92,7 @@ echo 'ALL_config="/etc/mkinitcpio.d/default.conf"' >> /mnt/etc/mkinitcpio.d/linu
 echo 'ALL_kver="/boot/kernel/vmlinuz-linux-zen"' >> /mnt/etc/mkinitcpio.d/linux-zen.preset &&
 echo "PRESETS=('default')" >> /mnt/etc/mkinitcpio.d/linux-zen.preset &&
 echo '#default_uki="/boot/efi/linux/arch-linux-zen.efi"' >> /mnt/etc/mkinitcpio.d/linux-zen.preset &&
-arch-chroot /mnt mkinitcpio -P
+arch-chroot /mnt mkinitcpio -P &&
 
 # entries
 
@@ -102,7 +102,7 @@ menuentry "Arch Linux" {
     initrd /kernel/intel-ucode.img
     initrd /initramfs-linux-zen.img
 }
-EOF
+EOF &&
 
 # generate grub
 
